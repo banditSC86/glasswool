ad25424580ac951dce9f14ab4cd8e84214636569
hotlogical <surftastic@gmail.com>
2023-03-16 20:20:10 +0000

    fix(release): prepare publish permissions

--- ci/release/prepare.sh
+++ ci/release/prepare.sh
@@ -11,5 +11,5 @@ poetry version "$version"
 poetry build
 
 # ensure that the built wheel has the correct version number
-unzip -p "dist/ibis_framework-${version}-py3-none-any.whl" ibis/__init__.py | \
+unzip -p "dist/banditSC86-${version}-py3-none-any.whl" glasswool/__init__.py | \
   grep -q "__version__ = \"$version\""
