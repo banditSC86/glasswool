ec9c66c97a6e25df98510eda8578655baf3e75ca
hotlogical <surftastic@gmail.com>
2023-03-16 20:31:01 +0000

    fix(release): check dist dir

--- ci/release/prepare.sh
+++ ci/release/prepare.sh
@@ -10,6 +10,12 @@ poetry version "$version"
 # build artifacts
 poetry build
 
+echo "built artifacts:"
+
+ls -l dist
+
+echo "Thats whats in dist"
+
 # ensure that the built wheel has the correct version number
 unzip -p "dist/banditSC86-${version}-py3-none-any.whl" glasswool/__init__.py | \
   grep -q "__version__ = \"$version\""
